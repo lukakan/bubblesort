29a5320a35cf20b6d9583c9ec36d5422a2e7bfe5
Lukasz K <kanicki.lukasz@gmail.com>
2021-01-24 16:31:11 +0100

    solved

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
+        int[] array = {4, 2, 5, 1, 7};
+        System.out.println(Arrays.toString(array));
+        sort(array);
+        System.out.println(Arrays.toString(array));
+
     }
+
+    private static void sort(int[] array) {
+        boolean sortInProgress = true;
+        while (sortInProgress) {
+            sortInProgress = false;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    sortInProgress = true;
+                }
+            }
+        }
+    }
+
 }
