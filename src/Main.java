94225928ae3422780dbe2d95e97919e38ceb09c5
Lukasz K <kanicki.lukasz@gmail.com>
2021-01-25 19:12:22 +0100

    corrected

--- src/Main.java
+++ src/Main.java
@@ -12,16 +12,24 @@ public class Main {
 
     private static void sort(int[] array) {
         boolean sortInProgress = true;
+        int iteration = 0;
+        int sortingRange = array.length;
         while (sortInProgress) {
             sortInProgress = false;
-            for (int i = 0; i < array.length - 1; i++) {
+            for (int i = 0; i < sortingRange - 1; i++) {
+                iteration++;
+                System.out.print("Porównanie " + iteration + ") " + array[i] + " vs " + array[i + 1]);
                 if (array[i] > array[i + 1]) {
                     int temp = array[i];
                     array[i] = array[i + 1];
                     array[i + 1] = temp;
+                    System.out.print(" - zmiana i tablica to teraz: " + Arrays.toString(array) + "\n");
                     sortInProgress = true;
+                } else {
+                    System.out.println();
                 }
             }
+            sortingRange--;
         }
     }
 
